541624e9f498ecd0fadd74dc16015dca8f480467
hiroo_laptop <qnrhd90@naver.com>
2025-07-29 13:16:07 +0900

    feat:소스 및 패키지 생성

---
+++ src/org/example/myapp/App.java
@@ -0,0 +1,11 @@
+package org.example.myapp;
+
+import org.example.util.Utils;
+
+/** 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다. <code>App</code> 클래스! */
+public class App {
+  /** 이 메서드는 entry point이다. */
+  public static void main(String[] args) {
+    Utils.printMessage("hello, world");
+  }
+}
