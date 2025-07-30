4d27479d1c24fb6ecf183ed116b919eaa6e96f96
hiroo_laptop <qnrhd90@naver.com>
2025-07-30 15:01:26 +0900

    feat:인텔리제이 및 gradle 활용

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,14 +1,15 @@
 package org.example.myapp;
 
-import com.google.common.base.Joiner;
-import org.example.util.Utils;
+import java.util.Scanner;
 
 /** 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다. <code>App</code> 클래스! */
 public class App {
   /** 이 메서드는 entry point이다. */
   public static void main(String[] args) {
-    Utils.printMessage("hello!");
-    String message = Joiner.on(", ").join("hello", "world");
-    System.out.println(message);
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("이름? ");
+    String name = scanner.nextLine();
+    System.out.printf("안녕하세요 %s 님!\n", name);
+    scanner.close();
   }
 }
