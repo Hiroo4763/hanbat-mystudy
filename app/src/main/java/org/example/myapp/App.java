08afe1a4dfd7e62b5ed8d121324603e6c870eddb
hiroo_laptop <qnrhd90@naver.com>
2025-07-31 12:54:47 +0900

    feat:spring boot 추가

--- app/src/main/java/org/example/myapp/App.java
+++ app/src/main/java/org/example/myapp/App.java
@@ -1,15 +1,10 @@
 package org.example.myapp;
 
-import java.util.Scanner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** 이 클래스는 한밭대학교 특강 실습 예제를 위한 첫 번째 클래스입니다. <code>App</code> 클래스! */
+@SpringBootApplication
 public class App {
-  /** 이 메서드는 entry point이다. */
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("이름? ");
-    String name = scanner.nextLine();
-    System.out.printf("안녕하세요 %s 님!\n", name);
-    scanner.close();
+    System.out.println("스프링부트 서버 시작!");
   }
 }
---
+++ app/src/main/java/org/example/myapp/App2.java
@@ -0,0 +1,7 @@
+package org.example.myapp;
+
+public class App2 {
+  public static void main(String[] args) {
+    System.out.println("스프링부트 서버 시작2!");
+  }
+}
